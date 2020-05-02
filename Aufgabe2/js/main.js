340dc4c53e4cb9fa015382d40a208f4f0652ece6
Nathalie Schneider <44029026+nathalieschn@users.noreply.github.com>
2020-05-02 14:06:33 +0200

    function shuffleCards

--- Aufgabe2/js/main.js
+++ Aufgabe2/js/main.js
@@ -1,9 +1,10 @@
 var A02;
 (function (A02) {
-    let allCards = [];
     let cards = [];
-    let values = ["hello", "Hallo", "goodbye", "Tschüss", "see", "sehen", "Corona", "Corona", "München", "Munich", "bed", "Bett", "bowl", "Schüssel", "card", "Karte", "flower", "Blume", "country", "Land", "dog", "Hund", "love", "Liebe", "blind", "Blind", "light", "Licht", "food", "Essen", "air", "Luft", "mood", "Stimmung", "despair", "EIA2", "cat", "Katze", "bird", "Vogel", "cup", "Tasse", "bottle", "Flasche", "dream", "Traum", "ear", "Ohr", "space", "Weltall", "tiger", "Tiger"];
+    let allCards = ["hello", "Hallo", "goodbye", "Tschüss", "see", "sehen", "Corona", "Corona", "München", "Munich", "bed", "Bett", "bowl", "Schüssel", "card", "Karte", "flower", "Blume", "country", "Land", "dog", "Hund", "love", "Liebe", "blind", "Blind", "light", "Licht", "food", "Essen", "air", "Luft", "mood", "Stimmung", "despair", "EIA2", "cat", "Katze", "bird", "Vogel", "cup", "Tasse", "bottle", "Flasche", "dream", "Traum", "ear", "Ohr", "space", "Weltall", "tiger", "Tiger"];
     let y;
+    let z;
+    let x;
     let anzahlc;
     function anzCards() {
         anzahlc = prompt("Anzahl der Kartenpaare eingeben 5-25");
@@ -13,8 +14,19 @@ var A02;
     let anzahlp;
     function anzPlayer() {
         anzahlp = prompt("Anzahl der Spieler eingeben");
-        y = parseInt(anzahlp);
+        z = parseInt(anzahlp);
     }
     anzPlayer();
+    function shuffleCards() {
+        for (let x = 0; x < y; x++) {
+            let randomCard = Math.floor((Math.random() * allCards.length));
+            cards.push(allCards[randomCard]);
+            let prodElement = document.createElement('div');
+            let karte = `<p class="${allCards[randomCard]}">${allCards[randomCard]}</p>`;
+            prodElement.innerHTML = karte;
+            document.getElementById("memory-card").appendChild(prodElement);
+            allCards.splice(randomCard, 1);
+        }
+    }
 })(A02 || (A02 = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe2/js/main.ts
+++ Aufgabe2/js/main.ts
@@ -1,10 +1,12 @@
 namespace A02 {
 
-    let allCards: string [] = [];
+    
     let cards: string[] = [];
     
-    let values: string[] = ["hello","Hallo","goodbye","Tschüss","see","sehen","Corona","Corona","München","Munich","bed","Bett","bowl","Schüssel","card","Karte","flower","Blume","country","Land","dog","Hund","love","Liebe","blind","Blind","light","Licht","food","Essen","air","Luft","mood","Stimmung","despair","EIA2","cat","Katze","bird","Vogel","cup","Tasse","bottle","Flasche","dream","Traum","ear","Ohr","space","Weltall","tiger","Tiger"]
+    let allCards: string[] = ["hello","Hallo","goodbye","Tschüss","see","sehen","Corona","Corona","München","Munich","bed","Bett","bowl","Schüssel","card","Karte","flower","Blume","country","Land","dog","Hund","love","Liebe","blind","Blind","light","Licht","food","Essen","air","Luft","mood","Stimmung","despair","EIA2","cat","Katze","bird","Vogel","cup","Tasse","bottle","Flasche","dream","Traum","ear","Ohr","space","Weltall","tiger","Tiger"]
     let y: number;
+    let z: number;
+    let x: number;
 
 let anzahlc: string
 function anzCards () : void {
@@ -16,8 +18,22 @@ anzCards();
 let anzahlp: string
 function anzPlayer () : void {
     anzahlp = prompt ("Anzahl der Spieler eingeben");
-    y = parseInt(anzahlp); 
+    z = parseInt(anzahlp); 
 }
 anzPlayer();
+
+function shuffleCards () {
+
+    for (let x: number = 0; x < y; x++) {
+        let randomCard = Math.floor((Math.random()*allCards.length));
+        cards.push(allCards[randomCard]);
+        let prodElement = document.createElement('div');
+            let karte: string= `<p class="${allCards[randomCard]}">${allCards[randomCard]}</p>`
+            prodElement.innerHTML = karte;
+            document.getElementById("memory-card").appendChild(prodElement);            
+            allCards.splice(randomCard, 1);
+    }
+}
+
 
 }
