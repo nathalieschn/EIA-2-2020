bf1a197f73f60c5f73ac9b57711a1e94ce90d93f
Nathalie Schneider <44029026+nathalieschn@users.noreply.github.com>
2020-05-16 15:35:49 +0200

    bestellungen funktioniert

--- Aufgabe3/js/main.js
+++ Aufgabe3/js/main.js
@@ -1,20 +1,45 @@
-var L03_StayHome;
-(function (L03_StayHome) {
+"use strict";
+var Haushaltshilfe;
+(function (Haushaltshilfe) {
     window.addEventListener("load", handleLoad);
-    function handleLoad(_event) {
-        console.log("Start");
-        let form = document.querySelector("div#form");
+    function handleLoad() {
+        let form = document.querySelector("#form");
         form.addEventListener("change", handleChange);
+        document.querySelector("#checkout")?.addEventListener("click", handleClick);
+    }
+    function handleClick() {
+        alert("Ihre Bestellung wird bearbeitet.");
     }
     function handleChange(_event) {
-        let order = document.querySelector("div#order");
+        //console.log(_event);
+        let order = document.querySelector("#order");
         order.innerHTML = "";
-        let formData = new FormData(document.forms[0]);
-        for (let entry of formData) {
-            let item = document.querySelector("[value='" + entry[1] + "']");
-            let price = Number(item.getAttribute("price"));
-            order.innerHTML += item.name + "  € " + price;
+        let data = new FormData(document.forms[0]);
+        let total = 0;
+        for (let entry of data) {
+            if (entry[0] == "Item") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                let price = Number(item.getAttribute("price"));
+                let amount = Number(data.get(entry[1] + "Menge"));
+                let einheit = (item.getAttribute("einheit"));
+                total += price * amount;
+                order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
+            }
+            if (entry[0] == "Service") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                let price = Number(item.getAttribute("price"));
+                let amount = Number(data.get(entry[1] + "Menge"));
+                let einheit = (item.getAttribute("einheit"));
+                total += price * amount;
+                order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
+            }
+            if (entry[0] == "Money") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                let betrag = Number(data.get("Betrag"));
+                order.innerHTML += item.value + ": " + betrag + " €" + " <br>";
+                total += 5;
+            }
         }
     }
-})(L03_StayHome || (L03_StayHome = {}));
+})(Haushaltshilfe || (Haushaltshilfe = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe3/js/main.ts
+++ Aufgabe3/js/main.ts
@@ -1,25 +1,58 @@
-namespace L03_StayHome {
-        window.addEventListener("load", handleLoad);
-    
-        function handleLoad(_event: Event): void {
-            console.log("Start");
-            let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
-    
-            form.addEventListener("change", handleChange);
-        }
-    
-        function handleChange(_event: Event): void {
+namespace Haushaltshilfe {
+    window.addEventListener("load", handleLoad);
 
-    
-            let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
-            order.innerHTML = "";
-    
-            let formData: FormData = new FormData(document.forms[0]);
-            for (let entry of formData) {
-                let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" +  entry[1] + "']");
+    function handleLoad(): void {
+        let form: HTMLDivElement = <HTMLDivElement> document.querySelector("#form");
+        form.addEventListener("change", handleChange);
+        document.querySelector("#checkout")?.addEventListener("click",handleClick);
+
+    }
+
+    function handleClick(): void {
+        alert("Ihre Bestellung wird bearbeitet.");
+    }
+
+    function handleChange(_event: Event): void {
+        //console.log(_event);
+        let order: HTMLDivElement = <HTMLDivElement> document.querySelector("#order");
+        order.innerHTML = "";
+
+        let data: FormData = new FormData(document.forms[0]);
+        let total: number = 0;
+
+        for (let entry of data) {
+            if (entry[0] == "Item") {
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
                 let price: number = Number(item.getAttribute("price"));
-    
-                order.innerHTML += item.name + "  € " + price;
+                let amount: number = Number (data.get(entry[1] + "Menge"));
+                let einheit: string = <string> (item.getAttribute("einheit"));
+                
+                total += price * amount;
+                order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
             }
+
+
+
+            if (entry[0] == "Service") {
+
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let price: number = Number(item.getAttribute("price"));
+                let amount: number = Number (data.get(entry[1] + "Menge"));
+                let einheit: string = <string> (item.getAttribute("einheit"));
+                
+                total += price * amount;
+                order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
             }
+
+            if (entry[0] == "Money") {
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let betrag: number = Number(data.get("Betrag"));
+                order.innerHTML += item.value + ": " +  betrag + " €" + " <br>" ;
+                total += 5;
+            }
+
+
+        }
+    }
+
 }
