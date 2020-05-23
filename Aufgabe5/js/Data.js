41ef0a58452856c6ed3b074dfd7322431c586203
Nathalie Schneider <44029026+nathalieschn@users.noreply.github.com>
2020-05-23 17:43:26 +0200

    Aufgabe 4 kopiert

---
+++ Aufgabe5/js/Data.js
@@ -0,0 +1,24 @@
+"use strict";
+var Haushaltshilfe;
+(function (Haushaltshilfe) {
+    Haushaltshilfe.data = {
+        Items: [
+            { name: "--", price: 0, einheit: "" },
+            { name: "Shampoo", price: 4.00, einheit: "Flaschen" },
+            { name: "Mehl", price: 2.50, einheit: "kg" },
+            { name: "Milk", price: 2.00, einheit: "Liter" },
+        ],
+        Supermarkt: [
+            { name: "Target", price: 0, einheit: "" },
+            { name: "Walmart", price: 0, einheit: "" },
+            { name: "Wholefoods", price: 0, einheit: "" },
+        ],
+        Service: [
+            { name: "--", price: 0, einheit: "" },
+            { name: "Cleaning", price: 15.00, einheit: "" },
+            { name: "Gardening", price: 17.00, einheit: "" },
+            { name: "Cooking", price: 23.00, einheit: "" },
+        ],
+    };
+})(Haushaltshilfe || (Haushaltshilfe = {}));
+//# sourceMappingURL=Data.js.map
